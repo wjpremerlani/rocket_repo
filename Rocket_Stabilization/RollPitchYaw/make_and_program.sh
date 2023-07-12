76ac2aaf34a61e79010109a03e04b1bce2575540
Joe Bowen <joe.b.bowen@gmail.com>
2023-07-12 17:07:28 -0600

    Add in the UDB6 Build files for UDB6 and Linux make and program.  Also created a bug fix for the logging time to increase accuracy

---
+++ Rocket_Stabilization/RollPitchYaw/make_and_program.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd RollPitchYaw-udb6.X
+
+# Recreate makefiles to make sure they match
+prjMakefilesGenerator -v -mplabx-userdir=~/.mplab_ide/dev/v6.05 ./
+
+make clean
+make all
+
+# Program
+pk2cmd -F dist/default/production/RollPitchYaw-udb6.X.production.hex -M -PdsPIC33FJ256GP710 -T -R -B.
+
+# Verify
+pk2cmd -F dist/default/production/RollPitchYaw-udb6.X.production.hex -PdsPIC33FJ256GP710 -Y -R -B.
+
+cd -
