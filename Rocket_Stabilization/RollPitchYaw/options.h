d0e5681f5cbc80b1ade53670417e43fd87bab4b9
William Premerlani <wjpremerlani@gmail.com>
2025-03-23 18:25:07 -0400

    version 23 of rocket stabilization : revised Wayne Robinson's gains for sn7 and 8

--- Rocket_Stabilization/RollPitchYaw/options.h
+++ Rocket_Stabilization/RollPitchYaw/options.h
@@ -9,13 +9,13 @@
 
 // the following allows multiple sets of options to be saved in one file
 #define FLORIN (0)
-#define JIM (1)
+#define JIM (0)
 #define BILL (0)
 #define WAYNE_BRD4 (0)
 #define WAYNE_BRD5 (0)
 #define WAYNE_BRD6 (0)
 #define WAYNE_BRD7 (0)
-#define WAYNE_BRD8 (0)
+#define WAYNE_BRD8 (1)
 
 // Florin's board
 #if( FLORIN == 1)
@@ -428,15 +428,15 @@
 
 #define BOARD "SN7, Wayne"
 #if (( GROUND_TEST == 1 ))
-#define DATE "4/15/2023, gnd test"
+#define DATE "3/23/25, gnd test"
 #else
-#define DATE "4/15/2023, flt rdy"
+#define DATE "3/23/25, flt rdy"
 #endif // GROUND_TEST 
 #define REVISION "R23, vert, apogee det"
 #define MAX_TILT_ANGLE ( 30.0 ) // degrees
 #define MAX_TILT_RATE ( 500.0 ) // degrees per second
 #define MAX_TILT_PULSE_WIDTH ( 400.0 ) // microseconds
-#define MAX_SPIN_RATE ( 1100.0 ) // degrees per second
+#define MAX_SPIN_RATE ( 2000.0 ) // degrees per second
 #define MAX_SPIN_PULSE_WIDTH ( 200.0 ) // microseconds
 #define MAX_ROLL_ANGLE ( 540 )
 
@@ -445,7 +445,7 @@
 
 // SN7
 #if ( WAYNE_BRD8 == 1 )
-#define GROUND_TEST (1)
+#define GROUND_TEST (0)
 #define USE_TILT (0)
 #define MOUNT_ORIENTATION VERTICAL_MOUNT
 //#define MOUNT_ORIENTATION HORIZONTAL_MOUNT
@@ -469,17 +469,17 @@
 #define CALIBRATION ( 1.0032 )
 
 #define BOARD "SN8, Wayne"
-#if (( GROUND_TEST == 0 ))
-#define DATE "2/11/2023, gnd test"
+#if (( GROUND_TEST == 1 ))
+#define DATE "3/23/25, gnd test"
 #else
-#define DATE "2/11/2023, flt rdy"
+#define DATE "3/23/25, flt rdy"
 #endif // GROUND_TEST 
 #define REVISION "R23, vert, apogee det"
 #define MAX_TILT_ANGLE ( 30.0 ) // degrees
-#define MAX_TILT_RATE ( 300.0 ) // degrees per second
-#define MAX_TILT_PULSE_WIDTH ( 500.0 ) // microseconds
-#define MAX_SPIN_RATE ( 900.0 ) // degrees per second
-#define MAX_SPIN_PULSE_WIDTH ( 300.0 ) // microseconds
+#define MAX_TILT_RATE ( 500.0 ) // degrees per second
+#define MAX_TILT_PULSE_WIDTH ( 400.0 ) // microseconds
+#define MAX_SPIN_RATE ( 2000.0 ) // degrees per second
+#define MAX_SPIN_PULSE_WIDTH ( 200.0 ) // microseconds
 #define MAX_ROLL_ANGLE ( 540 )
 
 #define GYRO_RANGE ( 1000 )
